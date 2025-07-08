9177281a2ca7df58803f6308796c15d9ffa850c5
PeerLabX <160556229+peerlabx@users.noreply.github.com>
2025-07-08 14:28:43 -0500

    Create lock.sh

---
+++ lock.sh
@@ -0,0 +1,8 @@
+# Lock screen on auto login
+# Start script below using Ubuntu "Startup Applications"
+# Use following command: 
+# /bin/bash -c "sleep 10 && /home/user/lock.sh"
+
+#!/bin/bash
+
+xdg-screensaver lock
